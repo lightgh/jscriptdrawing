a35b7ba75e4f4d33feb8e2d34fa0b2b57b506432
Chinaka Light <chinakalight@googlemail.com>
2017-09-14 02:24:44 +0100

    Simple javascript code

---
+++ test.js
@@ -0,0 +1,17 @@
+/**
+ * Author: Chinaka Light
+ * Aim: Learning javascrip
+ */
+console.log("hi welcome to javascrip");
+console.log("This is a sample javasxript application");
+var a = 20;
+
+for (var i = 0; i <= 20; i++) {
+  console.log(i);
+}
+
+var b = [34, 78, 990, 67];
+console.log("\n");
+for (var ba in b) {
+    console.log( b[ba]);
+}
